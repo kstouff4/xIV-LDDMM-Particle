78dac705d8b6da64a9fcb2b69e31b86eb7acf186
Kaitlin Stouffer <kstouff4@io61>
2023-02-10 03:38:55 -0500

    fixed output to view initial momentum and individual costs

---
+++ sandbox/fromScratchHamiltonian.py
@@ -0,0 +1,266 @@
+import os
+import time
+import numpy as np
+from numpy import random
+
+import torch
+from torch.autograd import grad
+
+import pykeops
+import socket
+pykeops.set_build_folder("~/.cache/keop" + pykeops.__version__ + "_" + (socket.gethostname()))
+
+from pykeops.torch import Vi, Vj
+
+np_dtype = "float32"
+dtype = torch.cuda.FloatTensor 
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+from matplotlib import pyplot as plt
+import matplotlib
+if 'DISPLAY' in os.environ:
+    matplotlib.use('qt5Agg')
+else:
+    matplotlib.use("Agg")
+    
+import sys
+from sys import path as sys_path
+sys_path.append('/cis/home/kstouff4/Documents/SurfaceTools/')
+import vtkFunctions as vtf
+
+
+# Kernels
+def GaussKernelHamiltonian(sigma,d):
+    qx,qy,px,py,wpx,wpy = Vi(0,d)/sigma, Vj(1,d)/sigma, Vi(2,d), Vj(3,d),Vi(4,1)/sigma,Vj(5,1)/sigma
+    h = 0.5*(px*py).sum() + wpy*((qx - qy)*px).sum() - (0.5) * wpx*wpy*(qx.sqdist(qy) - d)
+    D2 = qx.sqdist(qy)
+    K = (-D2 * 0.5).exp()
+    return (K*h).sum_reduction(axis=1) # N x M 
+
+def GaussLinKernel(sigma,d,l):
+    # u and v are the feature vectors 
+    x, y, u, v = Vi(0, d), Vj(1, d), Vi(2, l), Vj(3, l)
+    D2 = x.sqdist(y)
+    K = (-D2 / (2.0*sigma*sigma)).exp() * (u * v).sum() 
+    return (K).sum_reduction(axis=1)
+
+def GaussKernelB(sigma,d):
+    # b is px (spatial momentum)
+    x, y, b = Vi(0, d)/sigma, Vj(1, d)/sigma, Vj(2,d)
+    D2 = x.sqdist(y)
+    K = (-D2 * 0.5).exp()
+    return (K*b).sum_reduction(axis=1)
+
+
+###################################################################
+# Integration
+
+def RalstonIntegrator():
+    def f(ODESystem, x0, nt, deltat=1.0):
+        x = tuple(map(lambda x: x.clone(), x0))
+        dt = deltat / nt
+        l = [x]
+        for i in range(nt):
+            xdot = ODESystem(*x)
+            xi = tuple(map(lambda x, xdot: x + (2 * dt / 3) * xdot, x, xdot))
+            xdoti = ODESystem(*xi)
+            x = tuple(
+                map(
+                    lambda x, xdot, xdoti: x + (0.25 * dt) * (xdot + 3 * xdoti),
+                    x,
+                    xdot,
+                    xdoti,
+                )
+            )
+            l.append(x)
+        return l
+
+    return f
+
+#################################################################
+# Hamiltonian 
+def Hamiltonian(K0, sigma, d,numS):
+    # K0 = GaussKernelHamiltonian(x,x,px,px,w*pw,w*pw)
+    def H(p, q):
+        px = p[numS:].view(-1,d)
+        pw = p[:numS].view(-1,1)
+        qx = q[numS:].view(-1,d)
+        qw = q[:numS].view(-1,1) #torch.squeeze(q[:numS])[...,None]
+        
+        #h = 0.5*Vi(px)*Vj(px).sum() - (1.0/sigma) * Vj(pw)*Vj(qw)*((Vi(qx) - Vj(qx))*Vi(px)).sum() - (1.0/(2*sigma**2)) * Vi(pw*qw)*Vj(pw*qw)*(Vi(px).sqdist(Vj(px)) - d)
+        #print(h)
+        '''
+        H0 = (px * K0(qx,qx,px)).sum()
+        H1 = (px * K1(qx, qx, pw*qw)).sum()
+        H2 = (K2(qx,qx,pw*qw, pw*qw)).sum()
+        return 0.5 * H0 + H1 - 0.5*H2 # 0.5 * (px * K(qx, qx, px)).sum()
+        '''
+        #return ((h*K0(qx,qx)).sum_reduction(axis=1)).sum()
+        wpq = pw*qw
+        print("wpq shape")
+        print(wpq.detach().shape)
+        k = K0(qx,qx,px,px,wpq,wpq)
+        print("k is ")
+        print(k.detach().cpu().numpy())
+        return k.sum()
+
+    return H
+
+
+def HamiltonianSystem(K0, sigma, d,numS):
+    H = Hamiltonian(K0, sigma, d, numS)
+
+    def HS(p, q):
+        Gp, Gq = grad(H(p, q), (p, q), create_graph=True)
+        print("Gp and Gq are ")
+        print(Gp.detach().cpu().numpy())
+        print(Gq.detach().cpu().numpy())
+        return -Gq, Gp
+
+    return HS
+
+##################################################################
+# Shooting
+
+def Shooting(p0, q0, K0, K1, sigma,d, numS,nt=10, Integrator=RalstonIntegrator()):
+    return Integrator(HamiltonianSystem(K0,sigma,d,numS), (p0, q0), nt)
+
+
+def Flow(x0, p0, q0, K0, K1, sigma, d, deltat=1.0, Integrator=RalstonIntegrator()):
+    HS = HamiltonianSystem(K0,sigma,d,numS)
+
+    def FlowEq(x, p, q):
+        return (K1(x, q, p),) + HS(p, q)
+
+    return Integrator(FlowEq, (x0, p0, q0), deltat)[0]
+
+
+def LDDMMloss(K0, K1, sigma, d, numS,dataloss, gamma=1.0):
+    def loss(p0, q0):
+        p, q = Shooting(p0, q0, K0, K1, sigma, d,numS)[-1]
+        print("p,q after shooting ") 
+        print(p.detach().cpu().numpy())
+        print(q.detach().cpu().numpy())
+        return gamma * Hamiltonian(K0, sigma, d,numS)(p0, q0), dataloss(q)
+        #return dataloss(q)
+
+    return loss
+
+#################################################################
+# Data Attachment Term
+# K kernel for Varifold Norm (GaussLinKernel)
+def lossVarifoldNorm(T,w_T,zeta_T,zeta_S,K,d,numS,beta):
+    #print(w_T*zeta_T.cpu().numpy())
+    cst = (K(T,T,w_T*zeta_T,w_T*zeta_T)).sum()
+    print("cst is ")
+    print(cst.detach().cpu().numpy())
+
+    def loss(sS):
+        # sS will be in the form of q (w_S,S)
+        sSx = sS[numS:].view(-1,d)
+        sSw = sS[:numS].view(-1,1)
+        print("shapes of variables")
+        print(sSx.shape)
+        print(sSw.shape)
+        print(zeta_S.shape)
+        print(zeta_T.shape)
+        print(T.shape)
+        print(w_T.shape)
+     
+        k1 = K(sSx, sSx, sSw*zeta_S, sSw*zeta_S) 
+        print("ks")
+        print(k1.detach().cpu().numpy())
+        k2 = K(sSx, T, sSw*zeta_S, w_T*zeta_T)
+        print(k2.detach().cpu().numpy())
+              
+        return (
+            (beta/2.0)*(cst
+            + k1.sum()
+            - 2 * k2.sum())
+        )
+
+    return loss
+
+###################################################################
+# Optimization
+
+def makePQ(S,nu_S,T,nu_T):
+    # initialize state vectors based on normalization 
+    w_S = nu_S.sum(axis=-1)[...,None].type(dtype)
+    w_T = nu_T.sum(axis=-1)[...,None].type(dtype)
+    zeta_S = (nu_S/w_S).type(dtype)
+    zeta_T = (nu_T/w_T).type(dtype)
+    numS = w_S.shape[0]
+    q0 = torch.cat((w_S.clone().detach().flatten(),S.clone().detach().flatten()),0).requires_grad_(True).type(dtype)
+    print("q0 shape")
+    print(q0.shape)
+    p0 = (torch.zeros_like(q0)).requires_grad_(True).type(dtype)
+    
+    return w_S,w_T,zeta_S,zeta_T,q0,p0,numS
+
+def callOptimize(S,nu_S,T,nu_T,sigmaRKHS,sigmaVar,d,labs, savedir, its=100,beta=0.1):
+    w_S, w_T,zeta_S,zeta_T,q0,p0,numS = makePQ(S,nu_S,T,nu_T)
+    print("data types")
+    print(T.dtype)
+    print(w_T.dtype)
+    print(zeta_T.dtype)
+    dataloss = lossVarifoldNorm(T,w_T,zeta_T,zeta_S,GaussLinKernel(sigma=sigmaVar,d=d,l=labs),d,numS,beta=beta)
+    Kg = GaussKernelHamiltonian(sigma=sigmaRKHS,d=d)
+    Kv = GaussKernelB(sigma=sigmaRKHS,d=d)
+
+    loss = LDDMMloss(Kg,Kv,sigmaRKHS,d, numS, dataloss)
+
+    optimizer = torch.optim.LBFGS([p0], max_eval=10, max_iter=10,line_search_fn = 'strong_wolfe')
+    print("performing optimization...")
+    start = time.time()
+    
+    lossListH = []
+    lossListDA = []
+    def closure():
+        optimizer.zero_grad()
+        LH,LDA = loss(p0, q0)
+        L = LH+LDA
+        print("loss", L.detach().cpu().numpy())
+        lossListH.append(np.copy(LH.detach().cpu().numpy()))
+        lossListDA.append(np.copy(LDA.detach().cpu().numpy()))
+        L.backward()
+        return L
+    
+    for i in range(its):
+        print("it ", i, ": ", end="")
+        optimizer.step(closure)
+    print("Optimization (L-BFGS) time: ", round(time.time() - start, 2), " seconds")
+
+    f,ax = plt.subplots()
+    ax.plot(np.arange(len(lossListH)),np.asarray(lossListH),label='H($q_0$,$p_0$)')
+    ax.plot(np.arange(len(lossListH)),np.asarray(lossListDA),label='Varifold Norm')
+    ax.plot(np.arange(len(lossListH)),np.asarray(lossListDA)+np.asarray(lossListH),label='Total Cost')
+    ax.set_title("Loss")
+    ax.set_xlabel("Iterations")
+    ax.legend()
+    f.savefig(savedir + 'Cost.png',dpi=300)
+    
+    # Print out deformed states
+    listpq = Shooting(p0, q0, Kg, Kv, sigmaRKHS,d,numS)
+    Dlist = []
+    nu_Dlist = []
+    for t in range(10):
+        qnp = listpq[t][1]
+        D = qnp[numS:].detach().view(-1,d).cpu().numpy()
+        muD = qnp.detach().cpu().numpy()
+        nu_D = np.squeeze(muD[0:numS])[...,None]*zeta_S.detach().cpu().numpy()
+        Dlist.append(D)
+        nu_Dlist.append(nu_D)
+        # plot p0 as arrows
+    listSp0 = np.zeros((numS*2,3))
+    polyListSp0 = np.zeros((numS,3))
+    polyListSp0[:,0] = 2
+    polyListSp0[:,1] = np.arange(numS)# +1
+    polyListSp0[:,2] = numS + np.arange(numS) #+ 1
+    listSp0[0:numS,:] = S.detach().cpu().numpy()
+    listSp0[numS:,:] = p0[numS:].detach().view(-1,d).cpu().numpy() + listSp0[0:numS,:]
+    featsp0 = np.zeros((numS*2,1))
+    featsp0[numS:,:] = p0[0:numS].detach().view(-1,1).cpu().numpy()
+    vtf.writeVTK(listSp0,[featsp0],['p0_w'],savedir + 'testOutput_p0.vtk',polyData=polyListSp0)
+    return Dlist, nu_Dlist
--- sandbox/testRun.py
+++ sandbox/testRun.py
@@ -13,33 +13,40 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 def main():
     # create test dataset
-    N = 10
     d = 3
-    labs = 4
+    labs = 30
     sigmaRKHS = 2.0
     sigmaVar = 2.0
-    its = 20
+    its = 10
+    alpha = 3.0
+    beta = 1.0
+        
+    print("Parameters")
+    print("d: " + str(d))
+    print("labs: " + str(labs))
+    print("sigmaRKHS: " + str(sigmaRKHS))
+    print("sigmaVar: " + str(sigmaVar))
+    print("its: " + str(its))
+    print("alpha: " + str(alpha))
+    print("beta: " + str(beta))
 
     # make test dataset -- translation only 
-    x = torch.arange(-2,3).type(dtype)
+    x = torch.arange(-50,51).type(dtype)
     X,Y,Z = torch.meshgrid(x,x,x,indexing='ij')
     S = torch.stack((X.flatten(),Y.flatten(),Z.flatten()),axis=-1).type(dtype)
     N = S.shape[0]
-    print("number of particles is " + str(N))
+    print("N " + str(N))
     nu_S = np.zeros((N,labs))
     nu_S[np.arange(N),np.random.randint(low=0,high=labs,size=N)] = 1.0 # discrete particles 
     nu_S = torch.tensor(nu_S).type(dtype)
-    T = (torch.clone(S)*1.2).type(dtype)
-    nu_T = (torch.clone(nu_S)*(1.2)**d).type(dtype)
-    #T = T[:-1,:] # remove one particle so is M x N
-    #T[0,-1] += 0.3 # translate first particle in z
-    #T[-1,-1] -= 0.2 # translate last particle in z 
-    #nu_T = torch.clone(nu_S) # same exact features 
-    #nu_T = np.zeros((N,labs))
-    #nu_T[np.arange(N),np.random.randint(low=0,high=labs,size=N)] = 1.0 
-    #nu_T = torch.tensor(nu_T)
+    T = (torch.clone(S)*alpha).type(dtype)
+    nu_T = (torch.clone(nu_S)*(alpha)**d).type(dtype)
     
-    Dlist, nu_Dlist = callOptimize(S,nu_S,T,nu_T,torch.tensor(sigmaRKHS).type(dtype),torch.tensor(sigmaVar).type(dtype),d,labs,its=its)
+    savedir = '/cis/home/kstouff4/Documents/MeshRegistration/ParticleLDDMMQP/sandbox/output_dl_sig_its_albe_N-' + str(d) + str(labs) + '_' + str(sigmaRKHS) + str(sigmaVar) + '_' + str(its) + '_' + str(alpha) + str(beta) + '_' + str(N) + '/'
+    if (not os.path.exists(savedir)):
+        os.mkdir(savedir)
+
+    Dlist, nu_Dlist = callOptimize(S,nu_S,T,nu_T,torch.tensor(sigmaRKHS).type(dtype),torch.tensor(sigmaVar).type(dtype),d,labs,savedir,its=its,beta=beta)
     
     S=S.detach().cpu().numpy()
     T=T.detach().cpu().numpy()
@@ -55,8 +62,8 @@ def main():
         imageValsS.append(nu_S[:,i])
         imageValsT.append(nu_T[:,i])
 
-    vtf.writeVTK(S,imageValsS,imageNames,'testOutput_S.vtk',polyData=None)
-    vtf.writeVTK(T,imageValsT,imageNames,'testOutput_T.vtk',polyData=None)
+    vtf.writeVTK(S,imageValsS,imageNames,savedir+'testOutput_S.vtk',polyData=None)
+    vtf.writeVTK(T,imageValsT,imageNames,savedir+'testOutput_T.vtk',polyData=None)
     pointList = np.zeros((S.shape[0]*len(Dlist),d))
     polyList = np.zeros((S.shape[0]*(len(Dlist)-1),3))
     polyList[:,0] = 2
@@ -67,14 +74,34 @@ def main():
         imageValsD = [np.sum(nu_D,axis=-1), np.argmax(nu_D,axis=-1)]
         for i in range(labs):
             imageValsD.append(nu_D[:,i])
-        vtf.writeVTK(D,imageValsD,imageNames,'testOutput_D' + str(t) + '.vtk',polyData=None)
+        vtf.writeVTK(D,imageValsD,imageNames,savedir+'testOutput_D' + str(t) + '.vtk',polyData=None)
         if (t == len(Dlist) - 1):
-            np.savez('testOutput.npz',S=S, nu_S=nu_S,T=T,nu_T=nu_T,D=D,nu_D=nu_D)
+            np.savez(savedir+'testOutput.npz',S=S, nu_S=nu_S,T=T,nu_T=nu_T,D=D,nu_D=nu_D)
         else:
             pointList[int(t*len(D)):int((t+1)*len(D))] = D
             polyList[int(t*len(D)):int((t+1)*len(D)),1] = np.arange(t*len(D),(t+1)*len(D))
             polyList[int(t*len(D)):int((t+1)*len(D)),2] = np.arange((t+1)*len(D),(t+2)*len(D))
-    vtf.writeVTK(pointList,[],[],'testOutput_curves.vtk',polyData=polyList)
+    #polyList[:,1] = polyList[:,1]+1
+    #polyList[:,2] = polyList[:,2]+1
+    vtf.writeVTK(pointList,[],[],savedir+'testOutput_curves.vtk',polyData=polyList)
+    volS = np.prod(np.max(S,axis=(0,1)) - np.min(S,axis=(0,1)))
+    volT = np.prod(np.max(T,axis=(0,1)) - np.min(T,axis=(0,1)))
+    volD = np.prod(np.max(Dlist[-1],axis=(0,1)) - np.min(Dlist[-1],axis=(0,1)))
+    print("volumes of source, target, and deformed source")
+    print(volS)
+    print(volT)
+    print(volD)
+    print("total mass")
+    wS = np.sum(nu_S)
+    wT = np.sum(nu_T)
+    wD = np.sum(nu_Dlist[-1])
+    print(wS)
+    print(wT)
+    print(wD)
+    print("total mass per total volume")
+    print(wS/volS)
+    print(wT/volT)
+    print(wD/volD)
     return
 
 if __name__ == "__main__":
