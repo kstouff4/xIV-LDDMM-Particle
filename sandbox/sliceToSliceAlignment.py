fixing xc calculation in slice to slice alignment

sandbox/sliceToSliceAlignment.py
@@ -64,9 +64,13 @@ def applyRigid2D(S,nu_S,theta,tau):
     A[0,1] = -torch.sin(theta).type(dtype)
     A[1,0] = torch.sin(theta).type(dtype)
     
-    xc = torch.mean(torch.sum(nu_S,axis=-1)*S,axis=0)
+    xc = torch.sum((torch.sum(nu_S,axis=-1)[...,None]*S)/torch.sum(nu_S),axis=0)
+    print("xc: ", xc.detach())
     
-    Snew = (S - sc)@A.T + tau
+    Snew = (S - xc)@A.T + tau
+    print("min and max Snew")
+    print(torch.min(Snew.detach(),axis=0))
+    print(torch.max(Snew.detach(),axis=0))
     return Snew
 
 def getSlicesFromWhole(files,zU):
@@ -74,6 +78,9 @@ def getSlicesFromWhole(files,zU):
     info = np.load(files)
     X = info[info.files[0]]
     nu_X = info[info.files[1]]
+    print("min and max")
+    print(np.min(X,axis=0))
+    print(np.max(X,axis=0))
         
     zs = np.asarray(zU)
     D = (X[...,-1][...,None] - zs[None,...])**2
@@ -84,45 +91,104 @@ def getSlicesFromWhole(files,zU):
     for i in range(len(zU)):
         Slist.append(torch.tensor(X[slIndex == i,0:2]).type(dtype)) # ignore z 
         nu_Slist.append(torch.tensor(nu_X[slIndex == i,...]).type(dtype))
+        print("min and max of coordinates and nu_X")
+        print(np.min(X[slIndex == i,0:2],axis=0))
+        print(np.max(X[slIndex == i,0:2],axis=0))
+        print(np.min(nu_X[slIndex == i,...],axis=0))
+        print(np.max(nu_X[slIndex == i,...],axis=0))
     return Slist, nu_Slist, slIndex 
 
-def align(Slist,nu_Slist,sigma,its,savedir):
+def printTransformations(p0,savedir,it):
+    thetaTot = []
+    tauTotX = []
+    tauTotY = []
+    tot = 0
+    for i in range(0,p0.shape[0],3):
+        theta = p0[i].detach().cpu().numpy()
+        tau = p0[i+1:i+3].detach().cpu().numpy()
+        print("theta is, ", theta)
+        print("tau is, ", tau)
+        thetaTot.append(theta)
+        tauTotX.append(tau[0])
+        tauTotY.append(tau[1])
+        tot += 1
+    thetaTot = np.asarray(thetaTot)*180.0/np.pi
+    tauTotX = np.asarray(tauTotX)
+    tauTotY = np.asarray(tauTotY)
+    
+    f,ax = plt.subplots()
+    ax.plot(np.arange(len(thetaTot)),thetaTot,label="angle (deg): mean = {0:.6f}, std = {1:.6f}".format(np.mean(thetaTot),np.std(thetaTot)))
+    ax.plot(np.arange(len(tauTotX)),tauTotX,label="X translation (mm): mean = {0:.6f}, std = {1:.6f}".format(np.mean(tauTotX),np.std(tauTotX)))
+    ax.plot(np.arange(len(tauTotY)),tauTotY,label="Y translation (mm): mean = {0:.6f}, std = {1:.6f}".format(np.mean(tauTotY),np.std(tauTotY)))
+    ax.legend()
+    f.savefig(savedir + 'transformations_' + str(it) + '.png',dpi=300)
+    
+    f,ax = plt.subplots()
+    ax.plot(np.arange(len(thetaTot)),np.abs(thetaTot),label="angle (deg): mean = {0:.6f}, std = {1:.6f}".format(np.mean(np.abs(thetaTot)),np.std(np.abs(thetaTot))))
+    ax.plot(np.arange(len(tauTotX)),np.abs(tauTotX),label="X translation (mm): mean = {0:.6f}, std = {1:.6f}".format(np.mean(np.abs(tauTotX)),np.std(np.abs(tauTotX))))
+    ax.plot(np.arange(len(tauTotY)),np.abs(tauTotY),label="Y translation (mm): mean = {0:.6f}, std = {1:.6f}".format(np.mean(np.abs(tauTotY)),np.std(np.abs(tauTotY))))
+    ax.legend()
+    f.savefig(savedir + 'abstransformations_' + str(it) + '.png',dpi=300)
+
+            
+    print("sum of angles (deg) and tau")
+    print(np.sum(thetaTot))
+    print(np.sum(tauTotX))
+    print(np.sum(tauTotY))
+    print("average of angles and tau")
+    print(np.sum(thetaTot)/tot)
+    print(np.sum(tauTotX)/tot)
+    print(np.sum(tauTotY)/tot)
+    
+    return
+
+def align(Slist,nu_Slist,sigma,its,savedir,norm=False):
     
     K = GaussLinKernelSingle(sigma,2,nu_Slist[0].shape[-1])
-    p0 = torch.zeros((len(Slist)-2)*3).requires_grad_(True).type(dtype)
+    p0 = torch.zeros((len(Slist)-2)*3).type(dtype).requires_grad_(True)
+    print("p0, ", p0.detach())
     lossList = []
+    
+    if norm:
+        c = torch.tensor(10000.0).type(dtype)
+    else:
+        c = torch.tensor(1.0).type(dtype)
     
     def make_loss(Slist,nu_Slist):
         
         def loss(p0):
-            L = torch.tensor(0.0).type(dtype)
             for i in range(len(Slist)-2):
-                theta = p0[i*3]
-                tau = p0[1+i*3:(i+1)*3]
+                theta = p0[i*3].view(1,1)
+                tau = p0[1+i*3:(i+1)*3].view(1,2)
                 if (i == 0):
-                    L += K(Slist[i],applyRigid2D(Slist[i+1],nu_Slist[i+1],theta,tau),nu_Slist[i],nu_Slist[i+1]).sum()
+                    L = (1.0/c)*K(Slist[i],applyRigid2D(Slist[i+1],nu_Slist[i+1],theta,tau),nu_Slist[i],nu_Slist[i+1]).sum()
                 else:
-                    L += K(applyRigid2D(Slist[i],nu_Slist[i],p0[3*(i-1)],p0[1+(i-1)*3:i*3]),applyRigid2D(Slist[i+1],nu_Slist[i+1],theta,tau),nu_Slist[i],nu_Slist[i+1]).sum()
-            L += K(applyrigid2D(Slist[-2],nu_Slist[-2],p0[-3],p0[-2:]),Slist[-1],nu_Slist[-2],nu_Slist[-1]).sum() 
-            return L
+                    L += (1.0/c)*K(applyRigid2D(Slist[i],nu_Slist[i],p0[3*(i-1)].view(1,1),p0[1+(i-1)*3:i*3].view(1,2)),applyRigid2D(Slist[i+1],nu_Slist[i+1],theta,tau),nu_Slist[i],nu_Slist[i+1]).sum()
+                print("L intermediate, ", L.detach().cpu().numpy())
+            L += (1.0/c)*K(applyRigid2D(Slist[-2],nu_Slist[-2],p0[-3].view(1,1),p0[-2:].view(1,2)),Slist[-1],nu_Slist[-2],nu_Slist[-1]).sum()
+            return -2.0*L
         return loss
-            
-    def closure():
-        optimizer.zero_grad()
-        L = loss(p0)
-        print("loss, ", L.detach().cpu().numpy())
-        lossList.append(np.copy(L.detach().cpu().numpy()))
-        L.backward()
-        return L
     
     loss = make_loss(Slist,nu_Slist)
+    print("p0 ", p0.detach().cpu().numpy().shape)
     optimizer = torch.optim.LBFGS([p0], max_eval=15, max_iter=10,line_search_fn = 'strong_wolfe',history_size=100,tolerance_grad=1e-8,tolerance_change=1e-10)
     print("performing optimization...")
     start = time.time()
     
+    def closure():
+        optimizer.zero_grad()
+        Ln = loss(p0)
+        print("loss, ", Ln.detach().cpu().numpy())
+        lossList.append(np.copy(Ln.detach().cpu().numpy()))
+        Ln.backward()
+        return Ln
+    
     for i in range(its):
         optimizer.step(closure)
+        if (np.mod(i,10) == 0):
+            printTransformations(p0,savedir,i)
     
+    printTransformations(p0,savedir,its)
     f,ax = plt.subplots()
     ax.plot(np.arange(len(lossList)),np.asarray(lossList),label="Total Cost, Final = {0:.6f}".format(lossList[-1]))
     ax.legend()
@@ -131,7 +197,7 @@ def align(Slist,nu_Slist,sigma,its,savedir):
     Snew = [Slist[0]]
     thetas = [0]
     taus = [np.zeros(2)]
-    for i in range(len(Slist)-1):
+    for i in range(len(Slist)-2):
         Snew.append(applyRigid2D(Slist[i+1],nu_Slist[i+1],p0[i*3],p0[1+i*3:(i+1)*3]))
         thetas.append(p0[i*3].detach().cpu().numpy())
         taus.append(p0[1+i*3:(i+1)*3].detach().cpu().numpy())

sandbox/testSliceToSlice.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
+import sys
+import os
 from sys import path as sys_path
 sys_path.append('..')
 sys_path.append('../xmodmap')
@@ -14,31 +16,48 @@ import vtkFunctions as vtf
 import torch
 import sliceToSliceAlignment as sts
 
+np_dtype = "float32" #"float64"
+dtype = torch.cuda.FloatTensor #DoubleTensor 
+
+
 def main():
-    sigma = 1.0 # in mm 
-    its = 100
+    sigma = 0.5 # in mm 
+    its = 50
     
     wholeX = np.load('/cis/home/kstouff4/Documents/MeshRegistration/Particles/BarSeq/slicesAll_[28-56-111-101-47]_mmRedone_XoneHot.npz')
     zs = np.unique(wholeX[wholeX.files[0]][...,-1])
     
     filesO = ['/cis/home/kstouff4/Documents/MeshRegistration/Particles/BarSeq/Redo__optimalZnu_ZAllwC8.0_sig[0.2]_Nmax1500.0_Npart2000.0.npz']
     
-    Slist, nu_Slist, slIndex = sts.getSlicesFromWhole(filesO[0],zs)
+    extra = "BarSeq/"
+    
+    #wholeX = np.load('/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenMerfish/ZnuZ_Aligned/top20MI/sig0.1/All_ZnuZ_sig0.05.npz')
+    #zs = np.unique(wholeX[wholeX.files[0]][...,-1])
+    #filesO = ['/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenMerfish/ZnuZ_Aligned/top20MI/sig0.1/All_ZnuZ__optimalZnu_ZAllwC1.2_sig[0.1]_Nmax1500.0_Npart2000.0.npz']
+    
+    #extra = "AllenMerfish/"
                        
     original = sys.stdout
     
     savedir = '/cis/home/kstouff4/Documents/MeshRegistration/ParticleLDDMMQP/sandbox/SliceToSlice/'
     if (not os.path.exists(savedir)):
         os.mkdir(savedir) 
-
-    extra = "AllenMerfish/"
     
     savedir = savedir + extra
     
     if (not os.path.exists(savedir)):
         os.mkdir(savedir)
         
-    Snew, thetas, taus = sts.align(Slist,nu_Slist,sigma,its,savedir)
+    savedir = savedir + str(sigma) + '/'
+    if (not os.path.exists(savedir)):
+        os.mkdir(savedir)
+        
+    sys.stdout = open(savedir+'test.txt','w')
+    Slist, nu_Slist, slIndex = sts.getSlicesFromWhole(filesO[0],zs)
+    print(len(Slist))
+    print(len(nu_Slist))
+    
+    Snew, thetas, taus = sts.align(Slist,nu_Slist,torch.tensor(sigma).type(dtype),its,savedir,norm=False)
     totalPoints = slIndex.shape[0]
     SnewTotal = np.zeros((totalPoints,3))
     nuSTotal = np.zeros((totalPoints,nu_Slist[0].shape[-1]))