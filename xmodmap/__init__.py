1ef6a8e8259e0908a3aea0040bdb959c378281eb
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2023-07-27 23:02:31 +0200

    add normalize function

---
+++ xmodmap/__init__.py
@@ -0,0 +1,44 @@
+import torch
+
+
+import numpy as np
+
+def normalize(x, p=1., dim=1):
+    """
+    Normalize a tensor or numpy array along a given dimension. Return a copy of the input.
+
+    example:
+    >>> a = np.row_stack((np.zeros((3,2)), np.ones((1, 2))))
+    >>> normalize(a, p=2, dim=1)
+    >>> b = torch.tensor(a)
+    >>> normalize(b, p=2, dim=1)
+
+    """
+    if isinstance(x, torch.Tensor):
+        return torch.nn.functional.normalize(x, p=p, dim=dim)
+    else:
+        tmp = np.linalg.norm(x, ord=p, axis=dim, keepdims=True)
+        gd = tmp.squeeze() > 0
+        res = np.copy(x)
+        res[gd] = res[gd] / tmp[gd]
+        return res
+
+#
+# a = np.row_stack((np.zeros((3,2)), np.ones((1, 2))))
+# print(a)
+# print(a.__array_interface__['data'])
+#
+# b = normalize(a)
+# print(b)
+# print(b.__array_interface__['data'][0])
+# print(a)
+#
+#
+# c = torch.tensor(a)
+# print(c.data_ptr())
+#
+# d = normalize(c)
+# print(d.data_ptr())
+#
+# print(c)
+# print(d)
