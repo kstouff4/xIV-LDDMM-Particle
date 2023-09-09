fixing error in calculation of scale based on factor of d adn dimEff

xmodmap/deformation/Hamiltonian.py
@@ -115,9 +115,11 @@ class HamiltonianSystemGrid(Hamiltonian):
             getUdiv(self.sigma, self.d, self.uCoeff)(gx, qx, px, pw * qw) * gw
             + Alpha.sum() * gw
         )
+        print("Alpha shape: ", Alpha.shape)
+        print("Alpha sum: ", Alpha.sum())
 
         return -Gqx, -Gqw, Gpx, Gpw, Gg, Ggw
-
+    
 
 class HamiltonianSystemBackwards(Hamiltonian):
     """
@@ -135,8 +137,6 @@ class HamiltonianSystemBackwards(Hamiltonian):
         Gpx, Gpw, Gqx, Gqw = torch.autograd.grad(self.H(px, pw, qx, qw), (px, pw, qx, qw), create_graph=True)
         A, tau, Alpha = getATauAlpha(px, qx, pw, qw, cA=self.cA, cT=self.cT, dimEff=self.dimEff, single=self.single)
         xc = (qw * qx).sum(dim=0) / (qw.sum(dim=0))
-        print("xc2: ", xc)
-        print("weight shape: ", Gqw.shape)
         Tx = T #.view(-1, self.d)
         wTw = wT #.view(-1, 1)
         Gt = (

xmodmap/deformation/control/affine.py
@@ -19,16 +19,20 @@ def getATauAlpha(px, qx, pw, qw, cA=1.0, cT=1.0, dimEff=3, single=False):
         `Alpha`: is a diagonal matrix
     """
     xc = (qw * qx).sum(dim=0) / (qw.sum(dim=0))  # moving barycenters; should be 1 x 3
+    print("xc in get A tau alpha: ", xc)
     A = ((1.0 / (2.0 * cA)) * (px.T @ (qx - xc) - (qx - xc).T @ px))  # 3 x N * N x 3
     tau = ((1.0 / cT) * (px.sum(dim=0)))
-
-    if dimEff == 2:
-        alpha = ((px * (qx - xc)).sum() + (pw * qw * dimEff).sum())
+    if qx.shape[-1] == 2:
+        #print("d is 2")
+        alpha = ((px * (qx - xc)).sum() + (pw * qw).sum())
+        Alpha = torch.eye(2) * alpha
+    elif dimEff == 2:
+        alpha = (1.0/dimEff) * ((px * (qx - xc)).sum() + (pw * qw * dimEff).sum())
         Alpha = torch.eye(3) * alpha
         Alpha[-1, -1] = 0.0  # always scale Z by 0
     elif dimEff == 3 and single:
         print("dim Eff is 3 and single is True")
-        alpha = ((px * (qx - xc)).sum() + (pw * qw * dimEff).sum())
+        alpha = (1.0 / dimEff) * ((px * (qx - xc)).sum() + (pw * qw * dimEff).sum())
         Alpha = torch.eye(3) * alpha
     else:
         alpha_xy = 0.5 * (

xmodmap/deformation/control/nonRigid.py
@@ -13,6 +13,8 @@ def getU(sigma, d, uCoeff):
     Return:
         `U`: is a KeOps function
     """
+    if d == 3:
+        print("d 3 in getU")
     xO, qyO, py, wpyO = Vi(0, d), Vj(1, d), Vj(2, d), Vj(3, 1)
     # retVal = xO.sqdist(qyO)*torch.tensor(0)
     for sInd in range(len(sigma)):
@@ -24,7 +26,7 @@ def getU(sigma, d, uCoeff):
         if sInd == 0:
             retVal = (1.0 / uCoeff[sInd]) * K * h
         else:
-            retVal += (1.0 / uCoeff[sInd]) * (K * h)  # .sum_reduction(axis=1)
+            retVal += (1.0 / uCoeff[sInd]) * K * h  # .sum_reduction(axis=1)
     return retVal.sum_reduction(axis=1)  # G x 3
 
 
@@ -40,6 +42,8 @@ def getUdiv(sigma, d, uCoeff):
     Return:
         `Udiv`: is a KeOps function
     """
+    if d == 3:
+        print("d 3 in uDiv")
     xO, qyO, py, wpyO = Vi(0, d), Vj(1, d), Vj(2, d), Vj(3, 1)
     for sInd in range(len(sigma)):
         sig = sigma[sInd]
@@ -50,5 +54,5 @@ def getUdiv(sigma, d, uCoeff):
         if sInd == 0:
             retVal = (1.0 / (sig * uCoeff[sInd])) * K * h
         else:
-            retVal += (1.0 / (sig * uCoeff[sInd])) * (K * h)  # .sum_reduction(axis=1)
+            retVal += (1.0 / (sig * uCoeff[sInd])) * K * h  # .sum_reduction(axis=1)
     return retVal.sum_reduction(axis=1)  # G x 1

xmodmap/io/getOutput.py
@@ -217,12 +217,21 @@ def splitZs(Ti, nu_Ti, Di, nu_Di, savename, units=10, jac=None):
 
 
 def writeVTK(
-    YXZ, features, featureNames, savename, polyData=None, fields=None, fieldNames=None
+    YXZi, features, featureNames, savename, polyData=None, fields=None, fieldNames=None
 ):
     """
     Write YXZ coordinates (assume numpts x 3 as X,Y,Z in vtk file)
     polyData should be in format 3 vertex, vertex, vertex (0 based numbering)
     """
+    if YXZi.shape[-1] == 2:
+        if torch.is_tensor(YXZi):
+            YXZ = torch.zeros((YXZi.shape[0],3))
+            YXZ[:,0:2] = YXZi
+        else:
+            YXZ = np.zeros((YXZi.shape[0],3))
+            YXZ[:,0:2] = YXZi
+    else:
+        YXZ = YXZi
     f_out_data = []
 
     # Version 3.0 header